f4e41ec0437f7617b28f3b96b4abadb66599c076
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-08-21 17:39:23 +0530

    Updates

---
+++ precache-manifest.9e08034a3a2c55376e52ac74c318acad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb14b553ad0974208bfd4e2656565ea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e0971ac79b99bda0ef4d",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e0971ac79b99bda0ef4d",
+    "url": "/portfolio/static/js/2.ce28b5ac.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/portfolio/static/js/2.ce28b5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf866443e157eaff5858",
+    "url": "/portfolio/static/js/main.9623c2aa.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "37c969d7d67f9771dcce101d41611d4a",
+    "url": "/portfolio/static/media/memoji.37c969d7.png"
+  }
+]);
